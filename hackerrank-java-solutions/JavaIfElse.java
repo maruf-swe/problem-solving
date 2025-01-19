2842ca9d4e29f4c993af07651a517133db502fce
Maruf <marufur1847@gmail.com>
2025-01-20 00:02:40 +0600

    file name changed

--- hackerrank-java-solutions/JavaIfElse.java
+++ hackerrank-java-solutions/JavaIfElse.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class JavaIfElse {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         if (N % 2 == 0) {
