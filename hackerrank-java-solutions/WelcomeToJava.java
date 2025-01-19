3d2accea91bb690eda8bcda25651ba12d0e6e33d
Maruf <marufur1847@gmail.com>
2025-01-19 23:48:11 +0600

    first java hackerrank solution

---
+++ hackerrank-java-solutions/WelcomeToJava.java
@@ -0,0 +1,7 @@
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        // this is the first hackerrank problem.
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
