623103944faf9394a432417be970707a2790e7b9
Maruf <marufur1847@gmail.com>
2025-01-21 23:43:03 +0600

    twofloating number multiply

---
+++ geeksforgeeks-examples/GetInputUser.java
@@ -0,0 +1,19 @@
+package com.example.basics;
+
+import java.util.Scanner;
+
+class GetInputUser {
+    public static void main(String args[]) {
+        // Using Scanner for Getting Input from User
+        Scanner s = new Scanner(System.in);
+
+        String s1 = s.nextLine();
+        System.out.println("You entered string " + s1);
+
+        int a = s.nextInt();
+        System.out.println("You entered integer " + a);
+
+        float b = s.nextFloat();
+        System.out.println("You entered float " + b);
+    }
+}
---
+++ geeksforgeeks-examples/MultiplyTwoFloatingPointNumbers.java
@@ -0,0 +1,10 @@
+package com.example.basics;
+
+public class MultiplyTwoFloatingPointNumbers {
+    public static void main(String[] args) {
+        float number1 = 2.4f;
+        float number2 = 4.4f;
+        float multipliedNum = number1 * number2;
+        System.out.println("Multiplied Result is: " + multipliedNum);
+    }
+}
