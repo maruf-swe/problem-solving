92dc94798f4bb97e2872855fdc6b5d5a5487e9c0
Maruf <marufur1847@gmail.com>
2025-01-30 23:57:54 +0600

    parameter rectangular

---
+++ geeksforgeeks-examples/PerimeterOfRectangle.java
@@ -0,0 +1,27 @@
+package com.example.basics;
+
+public class PerimeterOfRectangle {
+    static void perimeter(int length, int breadth) {
+        // Calculate the 'perimeter' using the formula
+        int perimeter = 2 * (length + breadth);
+
+        System.out.println("The perimeter of the given rectangle of length "
+                + length + " and breadth " + breadth + " = "
+                + perimeter);
+    }
+
+    // Driver method
+    public static void main(String[] args) {
+        // Initialize a variable length that stores length of
+        // the given rectangle
+        int length = 10;
+
+        // Initialize a variable breadth that stores breadth
+        // of the given rectangle
+        int breadth = 20;
+
+        // Call the perimeter method on these length and
+        // breadth
+        perimeter(length, breadth);
+    }
+}
