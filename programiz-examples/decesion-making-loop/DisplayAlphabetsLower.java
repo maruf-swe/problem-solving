dbb4f4aae927e4ac09b5c70469091c2b17138b5c
Maruf <marufur1847@gmail.com>
2025-02-03 23:48:16 +0600

    alphabets lower

---
+++ programiz-examples/decesion-making-loop/DisplayAlphabetsLower.java
@@ -0,0 +1,9 @@
+package decesionmakingandloop;
+
+public class DisplayAlphabetsLower {
+    public static void main(String[] args) {
+        char c;
+        for (c = 'a'; c <= 'z'; ++c)
+            System.out.print(c + " ");
+    }
+}
