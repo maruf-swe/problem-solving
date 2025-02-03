041402dc41ae1f9fa8660fdfa48a207388fcf0b3
Maruf <marufur1847@gmail.com>
2025-02-03 23:53:16 +0600

    count number for loop

---
+++ programiz-examples/decesion-making-loop/CountNumberForLoop.java
@@ -0,0 +1,13 @@
+package decesionmakingandloop;
+
+public class CountNumberForLoop {
+    public static void main(String[] args) {
+
+        int count = 0, num = 123456;
+
+        for (; num != 0; num /= 10, ++count) {
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
