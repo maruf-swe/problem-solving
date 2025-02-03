62928fa18596652c0292a26cb14824c49586a7ac
Maruf <marufur1847@gmail.com>
2025-02-03 23:50:59 +0600

    count number

---
+++ programiz-examples/decesion-making-loop/CountNumber1.java
@@ -0,0 +1,16 @@
+package decesionmakingandloop;
+
+public class CountNumber1 {
+    public static void main(String[] args) {
+
+        int count = 0, num = 0003452;
+
+        while (num != 0) {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
